6f2e691a922ffe8c3cb05605ad491da7ffc05186
Dean <deanhume@gmail.com>
2016-04-05 17:02:33 +0100

    Added some MDL card logic

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,17 @@ fetch('./data/styles.json')
   // Loop through the results
   for(var i = 0; i < body.data.length; i++) {
     var style = body.data[i];
-      result += "<a href='/style.html?id=" + style.id + "'>" + style.name +  "</a><br>" + style.description + "<br><hr> ";
+      //result += "<a href='/style.html?id=" + style.id + "'>" + style.name +  "</a><br>" + style.description + "<br><hr> ";
+
+      // We want to keep a responsive grid in place
+      if (i % 3)
+      {
+        
+      }
+      result += '<div class="mdl-grid"></div>';
+
+      result += '<div class="mdl-cell mdl-cell--4-col"><div class="demo-card-square mdl-card mdl-shadow--2dp"><div class="mdl-card__title mdl-card--expand"><h2 class="mdl-card__title-text">Update</h2></div><div class="mdl-card__supporting-text">' + style.description + '</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/style.html?id="' + style.id + '">"' + style.name +  '"</a></div></div></div>';
 }
 
-  document.body.innerHTML = result;
+  document.getElementById("main").innerHTML = result;
 });
