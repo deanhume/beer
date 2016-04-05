0f8ff2758009678230f293818f1e57f59e5d7f7d
Dean <deanhume@gmail.com>
2016-04-05 10:50:14 +0100

    Added paging logic

--- src/js/style.js
+++ src/js/style.js
@@ -17,17 +17,25 @@ fetch('./data/styles.json')
 }).then(function(body) {
 
   var styleId = getParameterByName('id');
+  var pageId = getParameterByName('page');
   var style = body.data[styleId];
   var result = "<p>" + style.name +  "</p><br>" + style.description + "<br><hr> ";
 
+  // Check if we are being page
+  var styleUrl = './data/beers-style-' + styleId;
+  if (pageId > 0){
+    styleUrl += '-page-' + pageId + '.json';
+  }
+  else{
+    styleUrl += '.json';
+  }
+
   // Fetch the associated beers
   var beers = "";
-  var styleUrl = './data/beers-style-' + styleId  + '.json';
   fetch(styleUrl)
   .then(function(response) {
     return response.json();
   }).then(function(body) {
-
       // Loop through the results
       for(var i = 0; i < body.data.length; i++) {
         var beer = body.data[i];
@@ -37,9 +45,18 @@ fetch('./data/styles.json')
           {
             beers += "<img src='" + beer.labels.large + "' />";
           }
+
+          // Add a link to the brewery
+    }
+    result += beers;
+
+    // Get the number of pages to cycle through
+    if ((body.numberOfPages > 1) && (body.currentPage < body.numberOfPages))
+    {
+      const currentPage = body.currentPage + 1;
+      result += "<a href='/style.html?id=" + styleId + "&page=" + currentPage + "'>Next Page</a><br>";
     }
 
-    result += beers;
     document.body.innerHTML = result;
   });
 });
