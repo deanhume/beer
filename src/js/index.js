88313a28bd19dfd288b478d3140a6d71d47d2fd5
Dean <deanhume@gmail.com>
2016-04-13 12:17:28 +0100

    Added beer colours to cards

--- src/js/index.js
+++ src/js/index.js
@@ -10,21 +10,26 @@ fetch('./data/styles.json')
   // Loop through the results
   for(var i = 0; i < body.data.length; i++) {
     var style = body.data[i];
-      var cardDetails = '<div class="mdl-cell mdl-cell--4-col"><div class="demo-card-square mdl-card mdl-shadow--2dp"><div class="mdl-card__title mdl-card--expand"><h2 class="mdl-card__title-text">Update</h2></div><div class="mdl-card__supporting-text">' + style.name + '</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/style.html?id=' + style.id + '">Find out more</a></div></div></div>';
+      var cardDetails = '<div class="mdl-cell mdl-cell--4-col"><div class="demo-card-square mdl-card mdl-shadow--2dp"><div class="mdl-card__title mdl-card--expand {{beercolour}}"></div><div class="mdl-card__supporting-text">' + style.name + '</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/style.html?id=' + style.id + '">Find out more</a></div></div></div>';
 
-      // beer colour is determined by SRM (http://www.twobeerdudes.com/beer/srm)
+      // Beer colour is determined by SRM (http://www.twobeerdudes.com/beer/srm)
       if (style.srmMax < 10)
       {
         // pale
+        cardDetails = cardDetails.replace('{{beercolour}}','palebeer');
       }
-      else if (style.srmMax> 10 && style.srmMax<20)
+      else if (style.srmMax > 10 && style.srmMax < 20)
       {
         // amber
+        cardDetails = cardDetails.replace('{{beercolour}}','amberbeer');
+      }
+      else if (style.srmMax > 20){
+        // dark
+        cardDetails = cardDetails.replace('{{beercolour}}','darkbeer');
       }
       else{
-        // dark
+        cardDetails = cardDetails.replace('{{beercolour}}','palebeer');
       }
-
 
       // We want to keep a responsive grid in place
       if (innerCount % 3 === 0)
