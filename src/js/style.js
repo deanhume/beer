6398b4d00e2418b11b440248d5e33e7f7d209898
Dean <deanhume@gmail.com>
2016-04-13 14:02:12 +0100

    Added card layout to styles page

--- src/js/style.js
+++ src/js/style.js
@@ -31,7 +31,8 @@ fetch('./data/styles.json')
   }
 
   // Fetch the associated beers
-  var beers = "";
+  var innerCardDetails = "";
+  var innerCount = 1;
   fetch(styleUrl)
   .then(function(response) {
     return response.json();
@@ -39,24 +40,41 @@ fetch('./data/styles.json')
       // Loop through the results
       for(var i = 0; i < body.data.length; i++) {
         var beer = body.data[i];
-          beers += "<p>" + beer.name + "</p>";
-          // Display the label if we have one
-          if (beer.labels)
-          {
-            beers += "<img src='" + beer.labels.large + "' />";
-          }
 
-          // Add a link to the brewery
+        var cardDetails = '<div class="demo-card-square mdl-card mdl-shadow--2dp"><div class="mdl-card__title mdl-card--expand" style="{{beerimage}}"><h2 class="mdl-card__title-text">{{beername}}</h2></div><div class="mdl-card__supporting-text">{{beerdescription}}</div><div class="mdl-card__actions mdl-card--border"></div></div>';
+        cardDetails = cardDetails.replace('{{beername}}', beer.name);
+        cardDetails = cardDetails.replace('{{beerdescription}}', beer.description);
+
+        // Display the label if we have one
+        if (beer.labels)
+        {
+          cardDetails = cardDetails.replace('{{beerimage}}', "background: url('" + beer.labels.large + "') center / cover; height:300px;");
+        }
+        else{
+          cardDetails = cardDetails.replace('{{beerimage}}', "background: url('/images/srm/light.jpg') center / cover; height:300px;");
+        }
+
+        // We want to keep a responsive grid in place
+        if (innerCount % 3 === 0)
+        {
+          cardDetails = '<div class="mdl-grid">' + innerCardDetails + cardDetails; //add the opening tag
+          cardDetails = cardDetails + '</div>'; // closing tag
+          result += cardDetails;
+          innerCardDetails = ""; // reset the innercard details
+        }
+        else{
+          innerCardDetails += cardDetails;
+        }
+        innerCount++;
     }
-    result += beers;
 
     // Get the number of pages to cycle through
     if ((body.numberOfPages > 1) && (body.currentPage < body.numberOfPages))
     {
-      const currentPage = body.currentPage + 1;
+      var currentPage = body.currentPage + 1;
       result += "<a href='/style.html?id=" + styleId + "&page=" + currentPage + "'>Next Page</a><br>";
     }
 
-    document.body.innerHTML = result;
+    document.getElementById("main").innerHTML = result;
   });
 });
