c058efd7e2d057ca34fe878477797b73e05f8424
Dean <deanhume@gmail.com>
2016-06-09 17:51:08 +0100

    Added some styling love & updated beer page

--- src/js/beer.js
+++ src/js/beer.js
@@ -44,39 +44,56 @@ fetch(styleUrl)
     }
 
     // Add the details
-           var v = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"> \
-            <div class=\"mdl-card mdl-cell mdl-cell--12-col\"> \
-              <div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"> \
-              <h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Details</h4> \
-                <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-                  <div class=\"section__circle-container__circle mdl-color--primary\" style=\"margin-top: 35px;\"></div> \
-                </div> \
-                <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-                  <h5>International Bitterness Units scale</h5> \
-                  {{ibu}} \
-                </div> \
-                <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-                  <div class=\"section__circle-container__circle mdl-color--primary\" style=\"margin-top: 35px;\"></div> \
-                </div> \
-                <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-                  <h5>Alcohol by Volume</h5> \
-                  {{abvMin}} to {{abvMax}} \
-                </div> \
-                <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-                  <div class=\"section__circle-container__circle mdl-color--primary\" style=\"margin-top: 35px;\"></div> \
-                </div> \
-                <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-                  <h5>Final Gravity</h5> \
-                  {{fgMin}} to {{fgMax}} \
-                </div> \
-                <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-                  <div class=\"section__circle-container__circle mdl-color--primary\" style=\"margin-top: 35px;\"></div> \
-                </div> \
-                <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-                  <h5>Standard Reference Method</h5> \
-                  {{srmMin}} to {{srmMax}} \
-                </div> \
-              </div> \
-            </div> \
-          </section>";
+    // TODO: Not too happy about this large chunk of HTML!
+     var detailsTemplate = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"> \
+      <div class=\"mdl-card mdl-cell mdl-cell--12-col\"> \
+        <div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"> \
+        <h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Details</h4> \
+          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
+            <i class=\"material-icons orange600\">show_chart</i> \
+          </div> \
+          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
+            <h5>International Bitterness Units scale</h5> \
+            {{ibu}} \
+          </div> \
+          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
+            <i class=\"material-icons orange600\">opacity</i> \
+          </div> \
+          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
+            <h5>Alcohol by Volume</h5> \
+            {{abvMin}} to {{abvMax}} \
+          </div> \
+          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
+             <i class=\"material-icons orange600\">checkbox</i> \
+          </div> \
+          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
+            <h5>Final Gravity</h5> \
+            {{fgMin}} to {{fgMax}} \
+          </div> \
+          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
+             <i class=\"material-icons orange600\">palette</i> \
+          </div> \
+          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
+            <h5>Standard Reference Method</h5> \
+            {{srmMin}} to {{srmMax}} \
+          </div> \
+        </div> \
+      </div> \
+    </section>";
+
+    // Update the template
+    var beerDetails = htmlDocument.getElementById('details');
+    
+    detailsTemplate = detailsTemplate.replace('{{ibu}}', beer.ibu);
+    detailsTemplate = detailsTemplate.replace('{{abvMin}}', beer.style.abvMin);
+    detailsTemplate = detailsTemplate.replace('{{abvMax}}', beer.style.abvMax);
+    detailsTemplate = detailsTemplate.replace('{{fgMin}}', beer.style.fgMin);
+    detailsTemplate = detailsTemplate.replace('{{fgMax}}', beer.style.fgMax);
+    detailsTemplate = detailsTemplate.replace('{{srmMin}}', beer.style.srmMin);
+    detailsTemplate = detailsTemplate.replace('{{srmMax}}', beer.style.srmMax);
+
+    beerDetails.innerHTML = detailsTemplate;
+
+    // Kill the loading icon
+    htmlDocument.getElementById('loading-button').style.display = "none";
 });
