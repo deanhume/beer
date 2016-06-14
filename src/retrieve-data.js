53121b65e3fabcbf496aa6112475dfd9f2df3e14
Dean <deanhume@gmail.com>
2016-06-14 13:30:58 +0100

    Added synchronous fetch and it looks much better!

--- src/retrieve-data.js
+++ src/retrieve-data.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var request = require('request');
+var request = require('sync-request');
 var fs = require('fs');
 
 // The app
@@ -8,44 +8,32 @@ var app = express();
 
 app.use("/data", express.static(__dirname + '/data'));
 
-// Fetch the contents of each pageId
-function writeToDisk(theUrl, style, pageId){
-  request(theUrl, function (errorDetails, responseDetails, bodyDetails) {
-    if (!errorDetails && responseDetails.statusCode == 200) {
-      var fileName = './data/beers-style-' + style + '-page-' + pageId + '.json';
-      console.log('fileName', fileName);
-
-      // Write the body to file
-      fs.writeFile(fileName, bodyDetails);
-    }
-  });
-}
-
 // Get the home page
 app.get('/', function (req, res) {
 // Loop through each style
-for(var styleId = 1; styleId <= 1; styleId++) {
+for(var styleId = 4; styleId <= 20; styleId++) {
+
   var newBeerUrl = 'http://api.brewerydb.com/v2/beers?styleId=' + styleId + '&key=4f01238618e344bee9537ae5f5bb74cf&withBreweries=Y';
   console.log('Hitting this style Url:', newBeerUrl);
 
-  request(newBeerUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var beers = JSON.parse(body);
-      var totalPages = beers.numberOfPages;
+  var styleResults = request('GET', newBeerUrl);
+  var beers = JSON.parse(styleResults.getBody());
+  var totalPages = beers.numberOfPages;
 
-      // Loop through and get the beers per style
-      for(var pages = 1; pages <= totalPages; pages++) {
-        console.log('Looping through pages - on index:', pages);
+  // Loop through the results
+  for(var pages = 1; pages <= totalPages; pages++) {
+    console.log('Looping through pages - on index:', pages);
 
-        // Because we are zero based index
-        var pagedBeerUrl = newBeerUrl + '&p=' + pages;
-        writeToDisk(pagedBeerUrl, styleId, pages);
-      }
+    // Because we are zero based index
+    var pagedBeerUrl = newBeerUrl + '&p=' + pages;
+    var beerResults = request('GET', pagedBeerUrl);
+
+    var fileName = './data/beers-style-' + styleId + '-page-' + pages + '.json';
+    console.log('Writing fileName:', fileName);
+
+    // Write the body to file
+    fs.writeFile(fileName, beerResults.getBody());
     }
-    else{
-        console.log('Hit the limit for today');
-    }
-  });
   }
 });
 
