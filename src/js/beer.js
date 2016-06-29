814e09ed7545f5ed1071c17252b776230dc9a966
Dean <contact@deanhume.com>
2016-06-29 16:09:33 +0100

    Added toast notification on beer page

--- src/js/beer.js
+++ src/js/beer.js
@@ -45,6 +45,13 @@ function createStyleUrl(styleId, pageId)
   return styleUrl;
 }
 
+// If service workers are supported we are offline
+function showOfflineNotification(){
+  var snackbarContainer = document.querySelector('#offline-notification');
+  var data = {message: 'This page is now available offline'};
+  snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
+
 var styleId = getParameterByName('styleId');
 var pageId = getParameterByName('pageId');
 var beerId = getParameterByName('id');
@@ -57,7 +64,7 @@ fetch(styleUrl)
   return response.json();
 }).then(function(body) {
 
-    var beer = body.data[beerId]; console.log(beer);
+    var beer = body.data[beerId];
     var htmlDocument = document;
 
     // Append the style name in the hero image
@@ -80,42 +87,8 @@ fetch(styleUrl)
     }
 
     // Add the details
-    // TODO: Not too happy about this large chunk of HTML!
-     var detailsTemplate = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"> \
-      <div class=\"mdl-card mdl-cell mdl-cell--12-col\"> \
-        <div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"> \
-        <h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Details</h4> \
-          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-            <i class=\"material-icons orange600\">show_chart</i> \
-          </div> \
-          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-            <h5>International Bitterness Units scale</h5> \
-            {{ibu}} \
-          </div> \
-          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-            <i class=\"material-icons orange600\">opacity</i> \
-          </div> \
-          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-            <h5>Alcohol by Volume</h5> \
-            {{abvMin}} to {{abvMax}} \
-          </div> \
-          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-             <i class=\"material-icons orange600\">checkbox</i> \
-          </div> \
-          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-            <h5>Final Gravity</h5> \
-            {{fgMin}} to {{fgMax}} \
-          </div> \
-          <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-             <i class=\"material-icons orange600\">palette</i> \
-          </div> \
-          <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-            <h5>Standard Reference Method</h5> \
-            {{srmMin}} to {{srmMax}} \
-          </div> \
-        </div> \
-      </div> \
-    </section>";
+    // Not too happy about this large chunk of HTML!
+     var detailsTemplate = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"><div class=\"mdl-card mdl-cell mdl-cell--12-col\"><div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"><h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Details</h4><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\">show_chart</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>International Bitterness Units scale</h5>{{ibu}}</div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\">opacity</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Alcohol by Volume</h5>{{abvMin}} to {{abvMax}}</div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\">checkbox</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Final Gravity</h5>{{fgMin}} to {{fgMax}}</div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\">palette</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Standard Reference Method</h5>{{srmMin}} to {{srmMax}}</div></div></div></section>";
 
     // Update the template
     var beerDetails = htmlDocument.getElementById('details');
@@ -131,41 +104,7 @@ fetch(styleUrl)
     beerDetails.innerHTML = detailsTemplate;
 
     // The Brewery details
-    var breweryTemplate = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"> \
-     <div class=\"mdl-card mdl-cell mdl-cell--12-col\"> \
-       <div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"> \
-       <h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Brewery Info</h4> \
-         <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-           <i class=\"material-icons orange600\" style=\"padding-top: 20px;\">local_drink</i> \
-         </div> \
-         <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-           <h5>Brewery Name</h5> \
-           {{name}} \
-         </div> \
-         <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-           <i class=\"material-icons orange600\" style=\"padding-top: 20px;\">help</i> \
-         </div> \
-         <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-           <h5>About</h5> \
-           {{description}} \
-         </div> \
-         <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-            <i class=\"material-icons orange600\" style=\"padding-top: 20px;\">http</i> \
-         </div> \
-         <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-           <h5>Website</h5> \
-           <a href=\"{{website}}\">{{{website}}}</a> \
-         </div> \
-         <div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"> \
-            <i class=\"material-icons orange600\" style=\"padding-top: 10px;\">cake</i> \
-         </div> \
-         <div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"> \
-           <h5>Established</h5> \
-           {{established}} \
-         </div> \
-       </div> \
-     </div> \
-   </section>";
+    var breweryTemplate = "<section class=\"section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp\" style=\"margin-top: 50px;margin-bottom: 50px;width: 60%;\"><div class=\"mdl-card mdl-cell mdl-cell--12-col\"><div class=\"mdl-card__supporting-text mdl-grid mdl-grid--no-spacing\" style=\"padding-bottom: 30px;\"><h4 class=\"mdl-cell mdl-cell--12-col\" style=\"padding-top: 20px;\">Brewery Info</h4><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\" style=\"padding-top: 20px;\">local_drink</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Brewery Name</h5>{{name}}</div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\" style=\"padding-top: 20px;\">help</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>About</h5>{{description}}</div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\" style=\"padding-top: 20px;\">http</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Website</h5><a href=\"{{website}}\">{{{website}}}</a></div><div class=\"section__circle-container mdl-cell mdl-cell--2-col mdl-cell--1-col-phone\"><i class=\"material-icons orange600\" style=\"padding-top: 10px;\">cake</i></div><div class=\"section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone\"><h5>Established</h5>{{established}}</div></div></div></section>";
 
    // Update the template
    var breweryDetails = htmlDocument.getElementById('breweryDetails');
@@ -184,4 +123,6 @@ fetch(styleUrl)
 
   // Kill the loading icon
   htmlDocument.getElementById('loading-button').style.display = "none";
+
+  setTimeout(showOfflineNotification, 2000);
 });
