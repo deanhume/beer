bb8ea20b1a7f34097337e647cedcb69dad1a52a1
Dean <contact@deanhume.com>
2016-06-30 13:52:52 +0100

    Added base files

---
+++ retrieve-data.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var request = require('sync-request');
+var fs = require('fs');
+
+// The app
+var app = express();
+
+app.use("/data", express.static(__dirname + '/data'));
+
+// Get the home page
+app.get('/', function (req, res) {
+// Loop through each style
+for(var styleId = 41; styleId <= 50; styleId++) {
+
+  var newBeerUrl = 'http://api.brewerydb.com/v2/beers?styleId=' + styleId + '&key=4f01238618e344bee9537ae5f5bb74cf&withBreweries=Y';
+  console.log('Hitting this style Url:', newBeerUrl);
+
+  var styleResults = request('GET', newBeerUrl);
+  var beers = JSON.parse(styleResults.getBody());
+  var totalPages = beers.numberOfPages;
+
+  // Loop through the results
+  for(var pages = 1; pages <= totalPages; pages++) {
+    console.log('Looping through pages - on index:', pages);
+
+    // Because we are zero based index
+    var pagedBeerUrl = newBeerUrl + '&p=' + pages;
+    var beerResults = request('GET', pagedBeerUrl);
+
+    var fileName = './data/beers-style-' + styleId + '-page-' + pages + '.json';
+    console.log('Writing fileName:', fileName);
+
+    // Write the body to file
+    fs.writeFile(fileName, beerResults.getBody());
+    }
+  }
+
+  res.send('Finished processing');
+});
+
+// Open the server
+var server = app.listen(8019, function () {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://%s:%s', host, port);
+});
---
+++ service-worker.js
@@ -0,0 +1,114 @@
+(global => {
+  'use strict';
+
+  // Load the sw-toolbox library.
+  importScripts('bower_components/sw-toolbox/sw-toolbox.js');
+
+  // Turn on debug logging, visible in the Developer Tools' console.
+  global.toolbox.options.debug = false;
+
+  // We want to precache the following items
+  toolbox.precache([ '/index.html',
+                     '/about.html',
+                     '/style.html',
+                     '/beer.html',
+                     '/']);
+
+  // The route for any requests from the googleapis origin
+  toolbox.router.get('/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'googleapis',
+      maxEntries: 30,
+      maxAgeSeconds: 604800
+    },
+    origin: /\.googleapis\.com$/,
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'fonts',
+      maxEntries: 30,
+      maxAgeSeconds: 604800
+    },
+    origin: /\.gstatic\.com$/,
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/css/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'stylesheets',
+      maxEntries: 10,
+      maxAgeSeconds: 604800
+    },
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/images/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'images',
+      maxEntries: 10,
+      maxAgeSeconds: 604800
+    },
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/js/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'javascript',
+      maxEntries: 10,
+      maxAgeSeconds: 604800
+    },
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'beer-images-amazon',
+      maxEntries: 200,
+      maxAgeSeconds: 604800
+    },
+    origin: /\.amazonaws\.com$/,
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  toolbox.router.get('/data/(.*)', global.toolbox.cacheFirst, {
+    cache: {
+      name: 'beer-data',
+      maxEntries: 200,
+      maxAgeSeconds: 604800
+    },
+    // Set a timeout threshold of 2 seconds
+    networkTimeoutSeconds: 4
+  });
+
+  // Ensure that our service worker takes control of the page as soon as possible.
+  global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
+  global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
+})(self);
+
+function getFilenameFromUrl(path){
+    path = path.substring(path.lastIndexOf("/")+ 1);
+    return (path.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+}
+
+// Add in some offline functionality
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              var cachedFile = getFilenameFromUrl(event.request.url);
+              // Return the offline page
+              return caches.match(cachedFile);
+          })
+    );
+  }
+});
