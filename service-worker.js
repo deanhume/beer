ceb8259a84e76c3646f5b3bf819d1a2e39a810c5
Dean <contact@deanhume.com>
2016-07-26 10:44:06 +0100

    Updated with latest version

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,10 @@
   // Load the sw-toolbox library.
   importScripts('./bower_components/sw-toolbox/sw-toolbox.js');
 
+  // Load the Offline Google Analytics library
+  importScripts('./js/offline-google-analytics-import.js');
+  goog.offlineGoogleAnalytics.initialize();
+
   // Turn on debug logging, visible in the Developer Tools' console.
   global.toolbox.options.debug = false;
 
@@ -49,7 +53,7 @@
   toolbox.router.get('/beer/images/(.*)', global.toolbox.cacheFirst, {
     cache: {
       name: 'beer-images',
-      maxEntries: 10,
+      maxEntries: 300,
       maxAgeSeconds: 604800
     },
     // Set a timeout threshold of 2 seconds
@@ -87,51 +91,51 @@
     networkTimeoutSeconds: 4
   });
 
-  function getFilenameFromUrl(path){
-      path = path.substring(path.lastIndexOf("/")+ 1);
-      return (path.match(/[^.]+(\.[^?#]+)?/) || [])[0];
-  }
-
-  // Add in some offline functionality
-  this.addEventListener('fetch', event => {
-    // request.mode = navigate isn't supported in all browsers
-    // so include a check for Accept: text/html header.
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-          event.respondWith(
-            fetch(event.request.url).catch(error => {
-                var cachedFile = getFilenameFromUrl(event.request.url);
-                // Return the offline page
-                return caches.match(cachedFile);
-            })
-      );
-    }
-
-    // Check for WebP support
-    if (/\.jpg$|.gif$|.png$/.test(event.request.url)) {
-      // Inspect the accept header for WebP support
-      let supportsWebp = false;
-      if (event.request.headers.has('accept')){
-          supportsWebp = event.request.headers
-                                      .get('accept')
-                                      .includes('webp');
-      }
-
-      // If we support WebP
-      if (supportsWebp)
-      {
-          // Build the return URL
-          let returnUrl = event.request.url.substr(0, event.request.url.lastIndexOf(".")) + ".webp";
-
-          event.respondWith(
-            fetch(returnUrl, {
-              mode: 'no-cors'
-            })
-          );
-      }
-    }
-  });
-
   // Ensure that our service worker takes control of the page as soon as possible.
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
   global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
 })(self);
+
+function getFilenameFromUrl(path){
+    path = path.substring(path.lastIndexOf("/")+ 1);
+    return (path.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+}
+
+// Add in some offline functionality
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              var cachedFile = getFilenameFromUrl(event.request.url);
+              // Return the offline page
+              return caches.match(cachedFile);
+          })
+    );
+  }
+
+  // Check for WebP support
+  if (/\.jpg$|.gif$|.png$/.test(event.request.url)) {
+    // Inspect the accept header for WebP support
+    let supportsWebp = false;
+    if (event.request.headers.has('accept')){
+        supportsWebp = event.request.headers
+                                    .get('accept')
+                                    .includes('webp');
+    }
+
+    // If we support WebP
+    if (supportsWebp)
+    {
+        // Build the return URL
+        let returnUrl = event.request.url.substr(0, event.request.url.lastIndexOf(".")) + ".webp";
+
+        event.respondWith(
+          fetch(returnUrl, {
+            mode: 'no-cors'
+          })
+        );
+    }
+  }
+});
